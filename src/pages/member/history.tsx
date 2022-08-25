d6831524253b4bf70986aa4c4b4128383181f24c
Maria Saavedra <msaav3@gmail.com>
2022-08-25 17:39:15 -0500

    Added colors + created history page

---
+++ src/pages/member/history.tsx
@@ -0,0 +1,12 @@
+import ClassCardList from "../../components/ClassCardList/ClassCardList";
+import RegisterPage from "../../components/RegisterPage/RegisterPage";
+
+export default function History() {
+  return (
+    <>
+      <h1>History</h1>
+      <ClassCardList></ClassCardList>
+    </>
+  );
+}
+
